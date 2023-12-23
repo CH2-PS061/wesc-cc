e7b92749b010728a8b9a1f05949f6e675fb1f075
Tegar Joko Kurniawan <tegarjoke@gmail.com>
2023-12-23 15:26:01 +0700

    added db auth when server is started functionalities

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const usersRoutes = require("./routes/users");
 const featureRoutes = require("./routes/feature");
+const db = require("./config/db");
 
+app.get("/", async (req, res) => {
+  try {
+    await db.authenticate().then(() => {
+      res.send("<h1>Server is running, Database is running</h1>").status(200);
+    });
+  } catch (error) {
+    res.send(`<h1>Server is running,</h1><h1>Database has error : ${error}</h1>`).status(500);
+  }
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/v1/users", usersRoutes);
